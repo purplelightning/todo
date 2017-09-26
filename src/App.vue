85ad2fccca44ada364f8a80255e05f21eabb959e
purplelightning <1979506141@qq.com>
2017-09-26 20:28:28 +0800

    简单的实现列表

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,56 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <h1 v-html="title"></h1>
+    <ul>
+      <li v-for="item in items">
+        {{item.label}}
+      </li>
+    </ul>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+
+  export default {
+    data() {
+      return {
+        title: 'A Todo List',
+        items: [
+          {
+            label: '去游泳',
+            isFinished: false,
+          },
+          {
+            label: '去黄山',
+            isFinished: false,
+          },
+          {
+            label: '去丽江',
+            isFinished: true,
+          }
+        ]
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  ul{
+    margin:0;
+    padding:0;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+    font-size: 20px;
+  }
+  body{
+    width:100%;
+    background-color:rgba(23,180,52,0.6);
+  }
 </style>
